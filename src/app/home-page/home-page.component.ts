df1c55d249f8b50f65946b17ad79119885171d85
ValverdeC <corentin.valverde@gmail.com>
2017-05-21 00:59:03 +0200

    Add video on home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
-  constructor() { }
+	volume: string = "Off";
+	volumeIcon: string = "volume_off";
+	muted: boolean = true;
 
-  ngOnInit() {
-  }
+	volumeOnClick() {
+		if(this.volume == "Off") {
+			this.volume = "On";
+			this.volumeIcon = "volume_up";
+			this.muted = false;
+		} else {
+			this.volume = "Off";
+			this.volumeIcon = "volume_off";
+			this.muted = true;
+		}
+	}
 
 }

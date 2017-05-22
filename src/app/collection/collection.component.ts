66fb36b0a59586483d0ce07d7af03b5fc5ab45a1
ValverdeC <corentin.valverde@gmail.com>
2017-05-23 00:57:34 +0200

    Firsts tests on collection grid-list to conserve 3:2 ratio

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -10,9 +10,9 @@ import { Component } from '@angular/core';
 
 export class CollectionComponent {
 	tiles = [
-	    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-	    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-	    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-	    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+	    { cols: 3, rows: 2, photoUrl: './assets/img/gallery_test/IMG_9966.JPG'},
+	    { cols: 3, rows: 2, photoUrl: './assets/img/gallery_test/photo0.JPG'},
+	    { cols: 3, rows: 4.5, photoUrl: './assets/img/gallery_test/photo1.JPG'},
+	    { cols: 6, rows: 4, photoUrl: './assets/img/jabba.jpg'},
   	];
 }
